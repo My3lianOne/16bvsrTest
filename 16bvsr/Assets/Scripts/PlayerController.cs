17901e6392b0fce065731bc113a02a05df564f00
My3lian <my3lian1@gmail.com>
2019-01-07 16:56:13 +0300

    day1

--- 16bvsr/Assets/Scripts/PlayerController.cs
+++ 16bvsr/Assets/Scripts/PlayerController.cs
@@ -13,37 +13,84 @@ public class PlayerController : MonoBehaviour
     float jumpForce;
 
     [SerializeField]
+    float bounceForce;
+
+    [SerializeField]
     GameObject groundChecker;
+    [SerializeField]
+    GameObject wallChecker;
 
     Rigidbody2D rb;
+
+    private Vector2 direction;
+
+
+    float signDirection;
+    float normalDrag = 2;
+
+    float climbDrag = 10;
 
     public bool IsGrounded
     {
         get { return Physics2D.Linecast(transform.position, groundChecker.transform.position, 1 << LayerMask.NameToLayer("Ground")); }
     }
 
+    public bool IsClimb {
+        get { return Physics2D.Linecast(transform.position, wallChecker.transform.position, 1 << LayerMask.NameToLayer("Ground")) && !IsGrounded && Input.GetAxisRaw("Horizontal") != 0; }
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    // Start is called before the first frame update
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
+    void Update(){
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"),0);
+    }
     void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
+        Flip(h);
 
-        Vector3 move = new Vector3(h * moveSpd * Time.deltaTime, 0, 0);
+            rb.AddForce( direction * moveSpd * Time.deltaTime, ForceMode2D.Impulse);
 
-        transform.Translate(move);
+        if(Mathf.Abs(rb.velocity.x) > moveSpd/100f)
+		{
+			rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * moveSpd/100f, rb.velocity.y);
+		}
 
-        if (Input.GetButtonDown("Jump") && IsGrounded)
+        if(Input.GetButtonDown("Jump") && IsGrounded)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+            rb.velocity = new Vector2 (0, jumpForce);
+            //rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+
+        if(Input.GetButtonDown("Jump") && IsClimb)
+        {
+            //rb.velocity = new Vector2 (-transform.localScale.x * bounceForce, bounceForce/);
+             rb.AddForce(new Vector2 (-transform.localScale.x, 1) * bounceForce, ForceMode2D.Impulse);
+            Flip(-transform.localScale.x);
+        }
+    
+        if(IsClimb) {
+            rb.drag = climbDrag;
+        }
+        else {
+            rb.drag = normalDrag;
+        }
+    
+    }
+
+
+    private void Flip(float h) {
+        if (h != 0)
+            transform.localScale = new Vector3(h, transform.localScale.y, transform.localScale.z);
     }
 }
