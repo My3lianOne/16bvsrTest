3cee35f0479879bb31464517bd536e1bc4c157c0
My3lian <my3lian1@gmail.com>
2019-01-11 16:56:14 +0300

    world switch timing added

--- 16bvsr/Assets/Scripts/LevelSwitcher.cs
+++ 16bvsr/Assets/Scripts/LevelSwitcher.cs
@@ -1,12 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class LevelSwitcher : MonoBehaviour
 {
+    public enum WORLDS
+    {
+        Real = 0,
+        Pixel = 1
+    }
+
+    [FormerlySerializedAs("currentLevel")] public WORLDS currentWorld;
+
     [SerializeField] private float switchTime;
+
+    [SerializeField] 
+    private float switchCooldown;
+    [SerializeField] 
+    private float cooldownTimer;
+    
+    
+    [SerializeField] 
+    private bool canSwitch;
+    
     [SerializeField]
-    private GameObject normalLevel;
+    private GameObject realLevel;
     [SerializeField]
     private GameObject pixelLevel;
 
@@ -15,29 +35,90 @@ public class LevelSwitcher : MonoBehaviour
     private Animation fAnim;
 
     private bool isSwitching;
-    
+
+    [SerializeField]
+    private Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
         fAnim = fader.GetComponent<Animation>();
+
+        currentWorld = WORLDS.Real;        
+        
+        // Настройки слайдера
+        slider.maxValue = switchCooldown;
+        slider.value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && !isSwitching)
+        if (currentWorld == WORLDS.Pixel)
         {
-            fAnim.Play();
-            isSwitching = true;
-            Invoke("Switch", switchTime);
-            
-        }           
+            if (cooldownTimer <= 0)
+            {
+                Switch();
+            }
+            else
+            {
+                if (cooldownTimer > 0)
+                {
+                    cooldownTimer -= Time.deltaTime;
+                }                    
+            }
+                           
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Switch();
+            }
+        }
+
+        if (currentWorld == WORLDS.Real)
+        {           
+            if (cooldownTimer >= switchCooldown)
+            {
+                canSwitch = true;
+            }
+            else
+            {
+                if(cooldownTimer < switchCooldown)
+                    cooldownTimer += Time.deltaTime;
+            }
+        }
+        
+        if (Input.GetButtonDown("Fire1") && canSwitch && !isSwitching)
+        {                        
+            Switch();       
+        }
+
+        slider.value = cooldownTimer;
     }
 
     public void Switch()
-    {        
-        normalLevel.SetActive(!normalLevel.activeSelf);
-        pixelLevel.SetActive(!pixelLevel.activeSelf);
+    {
+        fAnim.Play();
+        
+        switch (currentWorld)
+        {
+            // Switch to PixelWorld
+            case WORLDS.Real:
+                realLevel.SetActive(false);
+                pixelLevel.SetActive(true);
+                currentWorld = WORLDS.Pixel;
+                canSwitch = true;
+                break;
+
+            // Switch to RealWorld
+            case WORLDS.Pixel:
+                realLevel.SetActive(true);
+                pixelLevel.SetActive(!pixelLevel.activeSelf);
+                currentWorld = WORLDS.Real;
+                canSwitch = false;
+                cooldownTimer = 0;
+                break;
+        }
+
         isSwitching = false;
     }
     
@@ -45,6 +126,4 @@ public class LevelSwitcher : MonoBehaviour
     {
         fader.SetActive(false);
     }
-    
-    
 }
