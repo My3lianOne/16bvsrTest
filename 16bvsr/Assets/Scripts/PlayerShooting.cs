0249b3ef3bd35a88eff3902f797c8b95bb50bc8a
My3lian <my3lian1@gmail.com>
2019-01-14 18:57:42 +0300

    shooting added (need complete)

---
+++ 16bvsr/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private int poolCapacity;
+
+    [SerializeField] private Transform shootingPlace;
+    
+    List<GameObject> pool = new List<GameObject>();
+
+    [SerializeField] private float shootForce; 
+
+    [SerializeField] private GameObject projectile;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < poolCapacity; i++)
+        {
+            GameObject obj = GameObject.Instantiate(projectile);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            SpawnProjectile();
+        }
+    }
+    
+    
+
+    GameObject SpawnProjectile()
+    {
+        foreach (var proj in pool)
+        {
+            if (proj.activeSelf == false)
+            {   
+                proj.SetActive(true);  
+                proj.transform.position = shootingPlace.position;
+                proj.transform.localScale = transform.localScale;
+                
+                
+                return proj;
+            }
+        }
+
+        return null;
+    }
+
+
+    
+    
+    
+}
---
+++ 16bvsr/Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float moveSpd;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start ()
+    {
+        Invoke(nameof(Deactivate), 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.right * transform.localScale.x * moveSpd;        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    
+    void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
