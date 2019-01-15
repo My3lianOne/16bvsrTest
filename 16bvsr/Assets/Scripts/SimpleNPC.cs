96daca38aeab2f00a846a09c58b3b13fa8f2a07c
My3lian <my3lian1@gmail.com>
2019-01-15 19:49:29 +0300

    start npc developement

---
+++ 16bvsr/Assets/Scripts/SimpleNPC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleNPC : MonoBehaviour
+{
+
+
+    [SerializeField] private float moveSpd;
+
+    private GameObject target;
+    
+    private Transform fieldOfView;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = Physics2D.OverlapArea(transform.up, fieldOfView.position, 1 << LayerMask.NameToLayer("Player")).gameObject;
+        if (target)
+        {
+            transform.LookAt(target);
+        }
+    }
+
+
+
+}
