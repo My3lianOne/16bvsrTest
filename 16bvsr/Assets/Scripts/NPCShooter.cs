43f185677bd2508db5f702d506933c9623dc3db8
My3lian <my3lian1@gmail.com>
2019-01-16 17:31:24 +0300

    Simple NPC added

---
+++ 16bvsr/Assets/Scripts/NPCShooter.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCShooter : MonoBehaviour
+{
+    [SerializeField] private int poolCapacity;
+
+    [SerializeField] private Transform shootingPlace;
+    
+    List<GameObject> pool = new List<GameObject>();
+
+    [SerializeField] private float shootForce; 
+
+    [SerializeField] private GameObject projectile;
+
+    [SerializeField] private float cooldown;
+    private bool canShoot;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        canShoot = true;
+        for (int i = 0; i < poolCapacity; i++)
+        {
+            GameObject obj = GameObject.Instantiate(projectile);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Shoot()
+    {
+        if (canShoot)
+        {
+            SpawnProjectile();
+        }
+    }
+
+    public GameObject SpawnProjectile()
+    {
+        foreach (var proj in pool)
+        {
+            if (proj.activeSelf == false)
+            {   
+                proj.SetActive(true);  
+                proj.transform.position = shootingPlace.position;
+                proj.transform.localScale = transform.localScale;
+
+                StartCoroutine(Recharge());
+                return proj;
+            }
+        }
+        return null;
+    }
+
+
+    private IEnumerator Recharge()
+    {
+        canShoot = false;
+
+        yield return new WaitForSeconds(cooldown);
+        
+        canShoot = true;
+    }
+}
--- 16bvsr/Assets/Scripts/SimpleNPC.cs
+++ 16bvsr/Assets/Scripts/SimpleNPC.cs
@@ -1,32 +1,118 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class SimpleNPC : MonoBehaviour
-{
+{    
+    [SerializeField]
+    private float moveSpd;
+    [SerializeField]
+    private GameObject target;    
+    [SerializeField] 
+    private Transform fieldOfView;
 
+    private NPCShooter shooter;
 
-    [SerializeField] private float moveSpd;
+    private Rigidbody2D rb;
 
-    private GameObject target;
+    private bool isIdle;
+
+    [SerializeField]
+    private float idleTime;
+
+    private float idleTimer;
     
-    private Transform fieldOfView;  
-    // Start is called before the first frame update
-    void Start()
+    
+
+
+    [SerializeField] private Transform groundChecker;
+
+    private void Start()
     {
-        
+        shooter = GetComponent<NPCShooter>();
+        rb = GetComponent<Rigidbody2D>();
+        isIdle = false;
+        idleTimer = idleTime;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        target = Physics2D.OverlapArea(transform.up, fieldOfView.position, 1 << LayerMask.NameToLayer("Player")).gameObject;
+    {                   
         if (target)
         {
-            transform.LookAt(target);
+            shooter.Shoot();
         }
+        else if (isIdle)
+        {
+            rb.velocity = Vector2.zero;
+            idleTimer -= Time.deltaTime;
+            if (idleTimer <= 0)
+            {
+                idleTimer = 0;
+                isIdle = false;
+                Flip();
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.right * (transform.localScale.x > 0 ? 1 : -1 ) * moveSpd;
+            if (!Physics2D.Linecast(transform.position, groundChecker.position, 1 << LayerMask.NameToLayer("Ground")))
+            {
+                isIdle = true;
+                idleTimer = idleTime;
+            }
+    
+        }
+        
+        
+        /*else if (isPatrol)
+        {
+            if (!Physics2D.Linecast(transform.position, groundChecker.position, 1 << LayerMask.NameToLayer("Ground")))
+                isPatrol = false;
+            rb.velocity = Vector2.right * (transform.localScale.x > 0 ? 1 : -1 ) * moveSpd;
+            patrolTimer -= Time.deltaTime;
+            if (patrolTimer <= 0)
+            {
+                patrolTimer = 0;
+                isPatrol = false;
+            }
+        }
+        else
+        {
+            patrolTimer -= Time.deltaTime;
+            if (patrolTimer >= patrolTime)
+            {
+                patrolTimer = patrolTime;
+                isPatrol = true;
+                Flip();
+            }
+            rb.velocity = Vector2.zero;
+        }*/
     }
 
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            target = other.gameObject;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            target = null;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 scale = transform.localScale;
+            transform.localScale = new Vector3(scale.x * -1, scale.y, scale.z);
+    }
 
 }
