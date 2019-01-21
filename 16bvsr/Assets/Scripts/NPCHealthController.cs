141cb8a60233f47bc14caa7a592fbfcd0e6ea2c7
My3lian <my3lian1@gmail.com>
2019-01-21 17:59:16 +0300

    докомит

---
+++ 16bvsr/Assets/Scripts/NPCHealthController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCHealthController : MonoBehaviour
+{
+    [Tooltip("Максимальные HP")]
+    [SerializeField]
+    private int maxHealth;   
+    
+    [Tooltip("Текущие HP")]
+    [SerializeField]
+    private int currentHealth;
+
+    [SerializeField] private GameObject gameController;
+    // Временно.
+     
+    
+    public enum ENEMY
+    {
+        Player,
+        NPC
+    }
+
+    [SerializeField] private ENEMY whoIsEnemy;
+    public bool IsDie { get; private set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        IsDie = false;
+        gameController = GameObject.FindWithTag("GameController");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Костыль. Сделать связь с геймконтроллером.
+        if (IsDie)
+        {
+            if (gameController)
+            {
+                
+            }
+            
+            transform.parent.gameObject.SetActive(false);
+        }    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            Hurt();
+    }
+
+    /// <summary>
+    /// Уменьшает количество HP на 1.
+    /// </summary>
+    public void Hurt()
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth--;
+            if (currentHealth <= 0)
+            {
+                IsDie = true;
+            }
+        }
+    }
+}
---
+++ 16bvsr/Assets/Scripts/Spawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSpawn;
+
+    [SerializeField] private float cooldown;
+
+    private GameObject instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = Instantiate(objectToSpawn);
+        instance.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        /*if (instance.activeSelf == false)
+        {
+            Invoke(nameof(Spawn), cooldown);
+        }*/
+    }
+
+    void Spawn()
+    {
+        instance.SetActive(true);
+        instance.transform.position = this.transform.position;
+    }
+    
+    void DeSpawn()
+    {
+        instance.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("GameBuffer"))
+        {
+            Spawn();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("GameBuffer"))
+        {
+            DeSpawn();
+        }
+    }
+}
