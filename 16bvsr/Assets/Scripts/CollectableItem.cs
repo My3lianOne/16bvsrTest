b998bea948601eeb0d474703d46a4d3c7a26be47
My3lian <my3lian1@gmail.com>
2019-01-23 12:33:15 +0300

    collectable items added AidKit added

---
+++ 16bvsr/Assets/Scripts/CollectableItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour, ICollectable
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            OnPick();
+        }
+    }
+
+    public virtual void OnPick()
+    {
+        Debug.Log("Picked");
+    }
+    
+    public virtual void Deactivate()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
